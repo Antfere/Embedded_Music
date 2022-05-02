7ca9742df791c87e36c3fa11773e72a948ed5d02
Antfere <anthonyatanasov3@gmail.com>
2022-05-02 13:23:05 +0300

    Functioning

--- client/src/components/Embed.js
+++ client/src/components/Embed.js
@@ -10,30 +10,36 @@ export class Embed extends Component {
 
     // Deprecated in heroku???
 
-    // makeEmbed = (url) => {
+    makeEmbed = (url) => {
+        
+        if (url.match(/soundcloud/) != null){
+            // Extra check just in case
+            if (url.match(/soundcloud/)[0] == "soundcloud") {
+                let Video = "https://w.soundcloud.com/player/?visual=true&url=" + url + "&auto_play=true&show_artwork=true"
+                return Video;
+            }
+        }
 
-    //     if (url.match(/soundcloud/) === "soundcloud") {
-    //         let Video = "https://w.soundcloud.com/player/?visual=true&url=" + url + "&auto_play=true&show_artwork=true"
-    //         return Video;
-    //     }
+        if (url.match(/youtube/) != null){
+            if (url.match(/youtube/)[0] == "youtube") {
+                let videoID = url.match(videoUrlToID)
+                let Video = "https://www.youtube.com/embed/" + videoID + "?autoplay=1" + "&rel=0" + "&vq=144p"
+                return Video;
+            }
+        }
 
-    //     if (url.match(/youtube/) === "youtube") {
-    //         let videoID = url.match(videoUrlToID)
-    //         let Video = "https://www.youtube.com/embed/" + videoID + "?autoplay=1" + "&rel=0" + "&vq=144p"
-    //         return Video;
-    //     }
-
-    // }
+    }
 
     render() {
 
         if (this.props.isOpen === true) return (
 
             <Container>
+                
+                {/* <iframe allow="autoplay" allowfullscreen="allowfullscreen" frameborder="0" loading="eager" importance="high" src={((this.props.item.url.match(/soundcloud/)) ? "https://w.soundcloud.com/player/?visual=true&url=" + this.props.item.url + "&auto_play=true" : "https://www.youtube.com/embed/" + this.props.item.url.match(/(?<=v=).{11}(?!list)/) + "?autoplay=1" + "&rel=0" + "&vq=144p")} onLoad={() => {console.log("gg")}}/>*/}
+                
                 {/* This works */}
-                <iframe allow="autoplay" allowfullscreen="allowfullscreen" frameborder="0" loading="eager" importance="high" src={((this.props.item.url.match(/soundcloud/)) ? "https://w.soundcloud.com/player/?visual=true&url=" + this.props.item.url + "&auto_play=true" : "https://www.youtube.com/embed/" + this.props.item.url.match(/(?<=v=).{11}(?!list)/) + "?autoplay=1" + "&rel=0" + "&vq=144p")} onLoad={() => {console.log("gg")}}/>
-                {/* This does not work in heroku */}
-                {/* <iframe allowfullscreen="allowfullscreen" frameborder="0" loading="eager" importance="high" src={this.makeEmbed(this.props.item.url)}/> */}
+                <iframe allowfullscreen="allowfullscreen" frameBorder="0" loading="eager" importance="high" src={this.makeEmbed(this.props.item.url)}/>
             </Container>
 
         )
